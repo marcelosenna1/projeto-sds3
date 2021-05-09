6197eddd5efc17e80587ed537b01416ad6ec3b27
Marcelo Sena <marcelomarcos2@gmail.com>
2021-05-09 16:14:09 -0300

    Alteração do título do Dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -10,14 +10,14 @@ const Dashboard = () => {
           <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary py-3">Dashboar de Vendas</h1>
+        <h1 className="text-primary py-3">Dashboar Vendas</h1>
 
         <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
             <BarChart />
           </div>
-
+      
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
             <DonutChart />
